a24a51ac1cee8618cde1bc8ff94d3f015388f928
Heather Nuffer <zzzuzz@gmail.com>
2019-11-26 23:17:30 -0700

    Displays followers

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -3,33 +3,58 @@ import './App.css';
 import axios from 'axios';
 import UserCard from './components/UserCard';
 
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      username: 'ethyl2',
       user: [],
-      followers: []
+      followers: [],
     }
   }
   componentDidMount() {
-    axios.get(' https://api.github.com/users/ethyl2')
+    axios.get(`https://api.github.com/users/${this.state.username}`)
     .then(response => {
-      console.log(response);
+      //console.log(response);
       this.setState({...this.state, user: response.data})
     })
-    
     .catch(err => console.log(err));
+
+    axios.get(`https://api.github.com/users/${this.state.username}/followers`)
+      .then(response => {
+        //console.log(response);
+        this.setState({...this.state, followers: response.data})
+        this.state.followers.forEach(follower => {
+          //console.log(follower.login);
+          axios.get(`https://api.github.com/users/${follower.login}`)
+          .then(response => {
+            //console.log(response);
+            this.setState({...this.state, [follower.login]: response.data})
+          })
+          .catch(err => console.log(err))
+        })   
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>GitHub Users</h1>
-      </header>
-      <UserCard user={this.state.user} /> 
-    </div>
-  );
+    //console.log(this.state);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>GitHub Users</h1>
+        </header>
+        <UserCard user={this.state.user} />
+        <h2>Followers:</h2> 
+        {this.state.followers.map(follower => {
+          return (
+            <UserCard user={follower} key={follower.id} />
+            )
+          }
+        )}
+      </div>
+    );
   }
 }
 

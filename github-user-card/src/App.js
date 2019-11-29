2a3ad7610f5ad0e67da2772513dfc9bfa793ba74
Heather Nuffer <zzzuzz@gmail.com>
2019-11-28 21:35:38 -0700

    Added form to get username and display that user's info

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -12,9 +12,42 @@ class App extends Component {
       username: 'ethyl2',
       user: [],
       followers: [],
+      lookupMessage: ''
     }
   }
 
+  submitUser = (event)  => {
+    event.preventDefault();
+    let submittedUser = event.target.newUser.value;
+    console.log('in submitUser');
+    console.log(submittedUser);
+    //console.log(event.target.newUser.value);
+    this.setState({...this.state, username: submittedUser});
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.username !== this.state.username) {
+      console.log('in componentDidUpdate');
+      axios.get(`https://api.github.com/users/${this.state.username}`)
+      .then(response => {
+        //console.log(response);
+        this.setState({...this.state, lookupMessage: 'Found user'});
+        this.setState({...this.state, user: response.data})
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({...this.state, lookupMessage: "Didn't find user. Check your username to make sure it is correct."});
+      });
+
+      axios.get(`https://api.github.com/users/${this.state.username}/followers`)
+        .then(response => {
+          //console.log(response);
+          this.setState({...this.state, followers: response.data})
+        })
+        .catch(err => console.log(err));
+    }
+  }
+  
   componentDidMount() {
     axios.get(`https://api.github.com/users/${this.state.username}`)
     .then(response => {
@@ -38,6 +71,14 @@ class App extends Component {
         <header className="App-header">
           <h1>GitHub Users</h1>
         </header>
+
+        <form onSubmit={this.submitUser}>
+          <label htmlFor='newUser'>Get GitHub User: </label>
+          <input type='text' name='newUser' id='newUser' placeholder='github username' />
+          <button type='submit'>Get User</button>
+        </form>
+        <p>{this.state.lookupMessage}</p>
+
         <UserCard user={this.state.user} />
         <h2 className='subheading'>Followers:</h2> 
         {this.state.followers.map(follower => {
